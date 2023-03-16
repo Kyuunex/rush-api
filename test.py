267a7de61b6a09c85278fea9e85816979c1f0902
Kyuunex <kyuunex@protonmail.ch>
2023-03-16 06:48:29 +0400

    update test.py

--- test.py
+++ test.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+import os
+
+DB_FILE = os.path.expanduser("~") + "Documents/rush.sqlite3"
+os.environ["RUSH_SQLITE_FILE"] = DB_FILE
+print("Database file is saved at: ", DB_FILE)
+
 from rushapi import app as application
 
 application.run(
